f7dcaf8110b058d85ad297a4909b70ed63e97ad6
Ernest-Geek <ernestampene1@gmail.com>
2023-05-16 14:47:44 +0000

    Implemented string to number conversion in C

--- string_to_number.c
+++ string_to_number.c
@@ -8,12 +8,12 @@ int main()
 	float x;
 	int y;
 
-	printf("String 1 is %s\n", str1);
-	printf("string 2 is %s\n", str2);
+	printf("String 1 is \"%s\"\n", str1);
+	printf("string 2 is \"%s\"\n", str2);
 
 	x = atof(str1); //converting string one to a float
 	y = atoi(str2); //converting string two to a float
 	
-	printf("String 1 converted to float is: %s\n", x);
-	printf("Sring 2 converted to an integer is: %s\n", y);
+	printf("String 1 converted to float is: %.2f\n", x);
+	printf("Sring 2 converted to an integer is: %d\n", y);
 }
